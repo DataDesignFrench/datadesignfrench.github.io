276ff42f15d73578c2dade378a14c93630dad00c
nychang <emailkathy@gmail.com>
2017-02-06 23:16:43 -0500

    Test document.write to insert html

--- js/htmlHead.js
+++ js/htmlHead.js
@@ -47,7 +47,7 @@ document.write('
   //   <meta name="keywords" content="data visualization, infographics, dataviz">
 
   //   <!-- <link rel="shortcut icon" href="favicon.ico" type="image/vnd.microsoft.icon"> -->
-  //   <!-- <link rel='stylesheet' href='//fonts.googleapis.com/css?family=font1|font2|etc' type='text/css'> -->
+  //   <!-- <link rel="stylesheet" href="//fonts.googleapis.com/css?family=font1|font2|etc" type="text/css"> -->
   //   <link rel="stylesheet" type="text/css" href="../theme/html/html.css">
 
   //   <script src="https://ajax.googleapis.com/ajax/libs/jquery/1.11.0/jquery.min.js" type="text/javascript"> </script>
