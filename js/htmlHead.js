e0990baad9eb40c1702c8ffa553bac64d1af754f
nychang <emailkathy@gmail.com>
2017-02-06 23:37:27 -0500

    Test document.write

--- js/htmlHead.js
+++ js/htmlHead.js
@@ -9,30 +9,26 @@
 // ');
 
 alert("load new content");
+console.log("HEEEEEEEEY");
 document.open();
-document.write("<h1>Out with the old - in with the new!</h1>");
-document.write('
-
-  <head>
-    <meta charset="UTF-8">
-    <meta name="viewport" content="width=device-width,initial-scale=1">
-
-    <title>Data + Design</title>
-    <meta name="author" content="name">
-    <meta name="description" content="A simple introduction to preparing and visualizing information">
-    <meta name="keywords" content="data visualization, infographics, dataviz">
-
-    <!-- <link rel="shortcut icon" href="favicon.ico" type="image/vnd.microsoft.icon"> -->
-    <!-- <link rel="stylesheet" href="//fonts.googleapis.com/css?family=font1|font2|etc" type="text/css"> -->
-    <link rel="stylesheet" type="text/css" href="../theme/html/html.css">
-
-    <script src="https://ajax.googleapis.com/ajax/libs/jquery/1.11.0/jquery.min.js" type="text/javascript"> </script>
-    <script src="../js/retina.min.js" type="text/javascript"> </script>
-    <script src="../js/data-design.js" type="text/javascript"> </script>
-  </head>
-
-
-
+document.write('\
+  <head>\
+    <meta charset="UTF-8">\
+    <meta name="viewport" content="width=device-width,initial-scale=1">\
+\
+    <title>Data + Design</title>\
+    <meta name="author" content="name">\
+    <meta name="description" content="A simple introduction to preparing and visualizing information">\
+    <meta name="keywords" content="data visualization, infographics, dataviz">\
+\
+    <!-- <link rel="shortcut icon" href="favicon.ico" type="image/vnd.microsoft.icon"> -->\
+    <!-- <link rel="stylesheet" href="//fonts.googleapis.com/css?family=font1|font2|etc" type="text/css"> -->\
+    <link rel="stylesheet" type="text/css" href="../theme/html/html.css">\
+\
+    <script src="https://ajax.googleapis.com/ajax/libs/jquery/1.11.0/jquery.min.js" type="text/javascript"> </script>\
+    <script src="../js/retina.min.js" type="text/javascript"> </script>\
+    <script src="../js/data-design.js" type="text/javascript"> </script>\
+  </head>\
 ');
 
 document.close();
