fb9606287c489b60ef5d4346ef21889b38c1f9d8
nychang <emailkathy@gmail.com>
2017-02-12 00:39:58 -0500

    Clean up files

---
+++ js/htmlNavigation-english.js
@@ -0,0 +1,133 @@
+document.write('\
+\
+    <span class="btn open">Open</span>\
+    <div class="navbar">\
+      <div class="title">\
+        <span class="btn close">Close</span>\
+        <h1>Data + Design</h1>\
+        <h2>A simple introduction to preparing and visualizing information</h2>\
+      </div>\
+\
+      <nav data-type="toc">\
+        <ol>\
+          <li data-type="part">\
+            <a href="LandingPage.html">Introduction</a>\
+            <ol>\
+              <li data-type="copyright-page">\
+                <a href="License-French.html">License</a>\
+              </li>\
+              <li data-type="preface">\
+                <a href="Preface-French.html">Preface</a>\
+              </li>\
+              <li data-type="foreword">\
+                <a href="Foreword-French.html">Foreword</a>\
+              </li>\
+              <li data-type="introduction">\
+                <a href="HowToUse-French.html">How to Use This Book</a>\
+              </li>\
+            </ol>\
+          </li>\
+\
+          <li data-type="part">\
+            <a href="DataFundamentals-French.html">Data Fundamentals</a>\
+            <ol>\
+              <li data-type="chapter">\
+                <a href="DataTypes-French.html">Basic Data Types</a>\
+              </li>\
+              <li data-type="chapter">\
+                <a href="Aggregation-French.html">About Data Aggregation</a>\
+              </li>\
+            </ol>\
+          </li>\
+\
+          <li data-type="part">\
+            <a href="CollectingData-French.html">Collecting Data</a>\
+            <ol>\
+              <li data-type="chapter">\
+                <a href="SurveyDesign-French.html">Intro to Survey Design</a>\
+              </li>\
+              <li data-type="chapter">\
+                <a href="SurveyQuestions-French.html">Types of Survey Questions</a>\
+              </li>\
+              <li data-type="chapter">\
+                <a href="CollectionMethods-French.html">Additional Data Collection Methods</a>\
+              </li>\
+              <li data-type="chapter">\
+                <a href="ExternalData-French.html">Finding External Data</a>\
+              </li>\
+            </ol>\
+          </li>\
+\
+          <li data-type="part">\
+            <a href="PreparingData-French.html">Preparing Data</a>\
+            <ol>\
+              <li data-type="chapter">\
+                <a href="GettingReady-French.html">Getting Data Ready</a>\
+              </li>\
+              <li data-type="chapter">\
+                <a href="DataCleaning-French.html">Data Cleaning</a>\
+              </li>\
+              <li data-type="chapter">\
+                <a href="DataChecks-French.html">Types of Data Checks</a>\
+              </li>\
+              <li data-type="chapter">\
+                <a href="CleaningCatch-French.html">What Data Cleaning Can and Can’t Catch</a>\
+              </li>\
+              <li data-type="chapter">\
+                <a href="Transformations-French.html">Data Transformations</a>\
+              </li>\
+            </ol>\
+          </li>\
+\
+          <li data-type="part">\
+            <a href="VisualizingData-French.html">Visualizing Data</a>\
+            <ol>\
+              <li data-type="chapter">\
+                <a href="DecidingIllustrate-French.html">Deciding Which and How Much Data to Illustrate</a>\
+              </li>\
+              <li data-type="chapter">\
+                <a href="GraphingCheckbox-French.html">Graphing the Results of Checkbox Responses</a>\
+              </li>\
+              <li data-type="chapter">\
+                <a href="AnatomyGraphic-French.html">Anatomy of a Graphic</a>\
+              </li>\
+              <li data-type="chapter">\
+                <a href="ColorFontIcons-French.html">Importance of Color, Font, and Icons</a>\
+              </li>\
+              <li data-type="chapter">\
+                <a href="PrintWeb-French.html">Print Vs. Web, Static Vs. Interactive</a>\
+              </li>\
+            </ol>\
+          </li>\
+\
+          <li data-type="part">\
+            <a href="WhatNotToDo-French.html">What Not To Do</a>\
+            <ol>\
+              <li data-type="chapter">\
+                <a href="Perception-French.html">Perception Deception</a>\
+              </li>\
+              <li data-type="chapter">\
+                <a href="CommonMistakes-French.html">Common Visualization\
+                Mistakes</a>\
+              </li>\
+            </ol>\
+          </li>\
+\
+          <li data-type="part">\
+            <a href="#">Conclusion</a>\
+            <ol>\
+              <li data-type="chapter">\
+                <a href="resources.html">Resources</a>\
+              </li>\
+              <li data-type="chapter">\
+                <a href="glossary.html">Glossary</a>\
+              </li>\
+              <li data-type="chapter">\
+                <a href="Contributors-French.html">Contributors</a>\
+              </li>\
+            </ol>\
+          </li>\
+        </ol>\
+      </nav>\
+    </div>\
+');
